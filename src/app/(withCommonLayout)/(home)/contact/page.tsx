4dc0acd905fa3549d3048476d6d65c3445f808f6
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2025-03-11 17:02:38 +0600

    contact section added

--- src/app/(withCommonLayout)/(home)/contact/page.tsx
+++ src/app/(withCommonLayout)/(home)/contact/page.tsx
@@ -1,6 +1,8 @@
 import Banner from "@/components/shared/banner";
 import PrimaryButton from "@/components/shared/PrimaryButton";
 import Image from "next/image";
+import logo from '../../../../assets/logo/logo-2.png'
+import Link from "next/link";
 
 export default function ContactPage() {
   return (
@@ -11,47 +13,33 @@ export default function ContactPage() {
         {/* Contact Info Section */}
         <section className="grid lg:grid-cols-2 gap-8 items-center mb-16">
           <div>
-            <h2 className="text-xl font-semibold text-[#1dd1a1] mb-2">
-              CONTACT
-            </h2>
-            <h3 className="text-4xl font-bold text-gray-800 mb-4">
-              Media and Business Inquiries
+            <h3 className="text-4xl font-bold text-[#001B61] mb-4">
+            Contact Info
             </h3>
-            <p className="text-gray-600 leading-relaxed mb-6">
-              Sagittis quisque ligula vitae dignissim vel nunc urna urna. Morbi
-              luctus purus vitae neque vitae pellentesque ultrices. Imperdiet at
-              pellentesque vitae aliquet tempor eget rutrum tellus.
+            <p className="text-[#001B61] leading-relaxed mb-6">
+            Welcome to our Website.
+            We are glad to have you around.Your email address will not be published.
+            Required fields are marked.
             </p>
             <div className="flex items-center gap-4 mb-4">
               <Image
-                src="/images/logo.png"
+                src={logo}
                 alt="Company Logo"
-                width={50}
-                height={50}
+                width={100}
+                height={100}
                 className="rounded-full"
               />
-              <div>
-                <p className="text-gray-800 font-bold">Skooli</p>
-                <p className="text-gray-600">info@Skooli.com</p>
-                <p className="text-gray-600">021-3456-789</p>
+              <div className="text-[#001B61]">
+                <p className="font-bold">Edu Skills</p>
+                <p>info@eduskills.com</p>
+                <p>031-4456-528</p>
               </div>
-            </div>
-            <div className="flex gap-4 text-blue-600">
-              <a href="#" className="hover:text-blue-800 transition">
-                <i className="fab fa-facebook-f"></i>
-              </a>
-              <a href="#" className="hover:text-blue-800 transition">
-                <i className="fab fa-twitter"></i>
-              </a>
-              <a href="#" className="hover:text-blue-800 transition">
-                <i className="fab fa-instagram"></i>
-              </a>
             </div>
           </div>
 
           <div>
-            <h3 className="text-xl font-semibold text-gray-800 mb-4">
-              Send us an inquiry below or email us at info@Skooli.com
+            <h3 className="text-xl font-semibold text-[#001B61] mb-4">
+            Send a Message
             </h3>
             <form className="bg-white shadow-md rounded-lg p-6 space-y-4">
               <input
--- src/components/modules/course/AllCourse.tsx
+++ src/components/modules/course/AllCourse.tsx
@@ -1,17 +1,14 @@
 import { ISubject } from '@/types'
-import CourseItem from '../home/course/CourseItem'
+import CourseCard from '../home/course/CourseCard'
 
 const AllSubject = ({ subject }: { subject: ISubject[] }) => {
   return (
     <div className="flex gap-8 my-10">
-      {/* <div className="w-full max-w-sm">
-        <FilterSidebar />
-      </div> */}
 
       <div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 ">
-          {subject?.map((product: ISubject, idx: number) => (
-            <CourseItem key={idx} data={product} />
+          {subject?.map((product: ISubject) => (
+            <CourseCard key={product._id} data={product} />
           ))}
         </div>
       </div>
